c2f1e17b7a78daa78a74015b461113af9e91e56d
maybenuo <maybenuo@outlook.com>
2018-04-21 09:31:13 +0800

    添加基础demo配置。

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ const CONF = {
     rootPathname: '',
 
     // 微信小程序 App ID
-    appId: '',
+    appId: 'wx9973d9d41cf22a39',
 
     // 微信小程序 App Secret
     appSecret: '',
@@ -21,7 +21,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: '',
+        pass: 'wx9973d9d41cf22a39',
         char: 'utf8mb4'
     },
 
